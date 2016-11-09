45c626712813cf70c8e5f1355102f2a7e233e0ba
Sean Counterman <counterman1128@aol.com>
2016-11-08 19:30:15 -0500

    Update ElevatorCarPiston.java
    Added the following: 
    -public static double pistonMove()
    -public static void setPistonState(Piston state)
    -public static Piston()
    -public static double up_time;
    -public static double down_time;
    -public static double currentPosition;
    -public static double time_sec()

--- ElevatorCarPiston.java
+++ ElevatorCarPiston.java
@@ -5,6 +5,16 @@ public class ElevatorCarPiston {
 	private int lastFloor;
 	private int destinationFloor;
 	
+	//Time variables for pistonMove()
+	public static double up_time;
+	public static double down_time;
+	//Position variable for pistonMove()
+	public static double currentPosition = 0;
+	
+	//Different states for piston object
+	public static enum Piston{MOVING_UP, STATIONARY, MOVING_DOWN}
+	public static Piston object;
+	
 	// Default Constructor
 	ElevatorCarPiston() {
 		currentFloor = 1;
@@ -57,8 +67,36 @@ public class ElevatorCarPiston {
 		}
 	}
 	
-	public boolean sentPistonToFisrtFloor() {
+	public boolean sentPistonToFirstFloor() {
 		currentFloor = 1;
 		return true;
+	}
+	
+	//Mutator for Piston State
+	public static void setPistonState(Piston state){
+		this = object.state;
+	}
+	
+	//Getter for Piston State
+	public static Piston(){
+		return object;
+	}
+	
+	//Calculates the position of the Piston with respect to time and speed
+	public static double pistonMove(){	
+		if(this == object.MOVING_UP){
+			up_time = time_sec();
+			currentPosition = currentPosition + 2.0 * Math.abs((up_time - down_time));
+		}else if(this == object.MOVING_DOWN){
+			down_time = time_sec();
+			currentPosition = currentPosition - 2.0 * Math.abs((up_time - down_time));
+		}
+		return currentPosition;
+	}
+	
+	//Converts system time to seconds
+	public static double time_sec(){
+		long elap_time = sys_time() - begin_time;
+		return (double)(elap_time / 1000000000);
 	}
 }

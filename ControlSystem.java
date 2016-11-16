acc0b176d8f1b70c155104a7c5537a80fb888a2f
Sean Counterman <counterman1128@aol.com>
2016-11-16 11:48:55 -0500

    Update ControlSystem.java

--- ControlSystem.java
+++ ControlSystem.java
@@ -47,19 +47,19 @@ public class ControlSystem {
 	public class ElevatorQueue implements Comaparator<Floor>{//May have to add function to check postion of floors compared to each other
 		@Override
 		public int compare(Floor obj1, Floor obj2){
-			if(piston.getPistonState == object.MOVING_UP){
-				if(obj1.getFloorPanelState > obj2.getFloorPanelState)
+			if(piston.getPistonState() == object.MOVING_UP){
+				if(obj1.getFloorPanelState() > obj2.getFloorPanelState())
 					return 1;
-				if(obj1.getFloorPanelState < obj2.getFloorPanelState)
+				if(obj1.getFloorPanelState() < obj2.getFloorPanelState())
 					return -1;
 			}
-			if(piston.getPistonState == object.MOVING_DOWN){
-				if(obj1.getFloorPanelState < obj2.getFloorPanelState)
+			if(piston.getPistonState() == object.MOVING_DOWN){
+				if(obj1.getFloorPanelState() < obj2.getFloorPanelState())
 					return 1;
-				if(obj1.getFloorPanelState > obj2.getFloorPanelState)
+				if(obj1.getFloorPanelState() > obj2.getFloorPanelState())
 					return -1;
 			}
-			if(piston.getPistonState == object.STATIONARY)
+			if(piston.getPistonState() == object.STATIONARY)
 				return 1;
 			return 0;
 		}

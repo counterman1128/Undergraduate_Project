bc2898c42ac88958dab7901747a2fca2d58e6384
ajw14 <ajwoodring@hotmail.com>
2016-11-16 16:04:35 -0500

    Add files via upload

---
+++ fireAlarm.java
@@ -0,0 +1,20 @@
+package elevatorProject;
+import java.util.*;
+
+	public class fireAlarm {
+		
+	private boolean AlarmOn;
+	
+	//Accessor 
+	public boolean AlarmOn(){
+		return AlarmOn;
+	}
+	//Mutator 
+	public void setAlarm (boolean value){
+		Alarm = value;
+	}
+	//Constructor
+	public fireAlarm(){
+		AlarmOn = false	
+	}
+}

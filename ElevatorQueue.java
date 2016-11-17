e9803a62c466693ca3129ca329a96347a23e9c36
Sean Counterman <counterman1128@aol.com>
2016-11-17 14:47:32 -0500

    Update ElevatorQueue.java

--- ElevatorQueue.java
+++ ElevatorQueue.java
@@ -1,27 +1,5 @@
 package Undergraduate_Project;
 import Undergraduate_Project.Piston;
-import Undergraduate_Project.ControlSystem;
+import Undergraduate_Project.ElevatorCarPiston;
 import java.util.*;
 //Comparator Class
-public class ElevatorQueue implements Comparator<Floor>{//May have to add function to check postion of floors compared to each other
-	public Piston object;
-	
-	@Override
-	public int compare(Floor obj1, Floor obj2){
-		if(piston.getPistonState() == object.MOVING_UP){
-			if(obj1.getFloorPanelState() > obj2.getFloorPanelState())
-				return 1;
-			if(obj1.getFloorPanelState() < obj2.getFloorPanelState())  
-				return -1;
-		}
-		if(piston.getPistonState() == object.MOVING_DOWN){
-			if(obj1.getFloorPanelState() < obj2.getFloorPanelState())
-				return 1;
-			if(obj1.getFloorPanelState() > obj2.getFloorPanelState())
-				return -1;
-		}
-		if(piston.getPistonState() == object.STATIONARY)
-			return 1;
-		return 0;
-	}
-}

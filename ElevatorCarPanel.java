fa148ac057bb27e6c26fb7ab287872ef40401d3c
Sean Counterman <counterman1128@aol.com>
2016-11-17 16:09:17 -0500

    Update ElevatorCarPanel.java

--- ElevatorCarPanel.java
+++ ElevatorCarPanel.java
@@ -33,7 +33,7 @@ public class ElevatorCarPanel {
     ElevatorCarPanel() {
     	emergencyMode = false;
     	maintenanceMode = false;
-    	destinationFloor = new LinkedList();
+    	destinationFloor = new LinkedList()<Integer>;
     }
 
 

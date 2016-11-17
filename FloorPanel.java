52c1dcd9e8f666481d24badc1eeab955080b0f7c
Sean Counterman <counterman1128@aol.com>
2016-11-17 17:03:32 -0500

    Update FloorPanel.java

--- FloorPanel.java
+++ FloorPanel.java
@@ -4,10 +4,12 @@ import Undergraduate_Project.FloorPanelState;
 public class FloorPanel {
 	private Boolean upButtonPressed;
 	private Boolean downButtonPressed;
-	
+	public FloorPanelState state;
+	public Object panel = new Object();
 	FloorPanel() {
 		upButtonPressed = false;
 		downButtonPressed = false;
+		panel=state.VOID;
 	}
 	
 	Boolean getUpButtonPressed() {
@@ -20,9 +22,23 @@ public class FloorPanel {
 	
 	void setUpButtonPressed(Boolean newState) {
 		upButtonPressed = newState;
+		downButtonPressed = !newState;
+		if(newState)
+			panel = state.UP;
+		else
+			panel = state.DOWN;
 	}
 	
 	void setDownButtonPressed(Boolean newState) {
 		downButtonPressed = newState;
+		upButtonPressed = !newState;
+		if(newState)
+			panel = state.DOWN;
+		else
+			panel = state.UP;
+	}
+	
+	public Object getPanelState(){
+		return panel;	
 	}
 }

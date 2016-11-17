85590001aefc2cf8317599798d44035c12f8a879
Sean Counterman <counterman1128@aol.com>
2016-11-17 17:40:10 -0500

    Update ControlSystem.java

--- ControlSystem.java
+++ ControlSystem.java
@@ -58,7 +58,7 @@ public class ControlSystem {
 					return 1;
 				if(obj1.getFloorPanelState() < obj2.getFloorPanelState())  
 					return -1;
-				if(obj1.floorPosition < obj2.floorPosition && obj1.returnPanelStatus() == state.UP && obj.floorPosition > piston.getCurrentPosition())
+				if(obj1.floorPosition < obj2.floorPosition && obj1.returnPanelStatus() == state.UP && obj1.floorPosition > piston.getCurrentPosition())
 					return 1;
 				else if(obj2.returnPanelStatus() == state.UP && obj2.floorPosition > piston.getCurrentPosition())
 					return -1;
@@ -69,7 +69,7 @@ public class ControlSystem {
 					return 1;
 				if(obj1.getFloorPanelState() > obj2.getFloorPanelState())
 					return -1;
-				if(obj1.floorPosition > obj2.floorPosition && obj1.returnPanelStatus() == state.DOWN && obj.floorPosition < piston.getCurrentPosition())
+				if(obj1.floorPosition > obj2.floorPosition && obj1.returnPanelStatus() == state.DOWN && obj1.floorPosition < piston.getCurrentPosition())
 					return 1;
 				else if(obj2.returnPanelStatus() == state.DOWN && obj2.floorPosition < piston.getCurrentPosition())
 					return -1;

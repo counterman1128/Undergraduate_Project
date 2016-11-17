6ff49391007cfaf04bc6e399c0ea140fdfb2a4e1
Sean Counterman <counterman1128@aol.com>
2016-11-17 17:36:23 -0500

    Update ControlSystem.java

--- ControlSystem.java
+++ ControlSystem.java
@@ -58,9 +58,9 @@ public class ControlSystem {
 					return 1;
 				if(obj1.getFloorPanelState() < obj2.getFloorPanelState())  
 					return -1;
-				if(obj1.floorPosition < obj2.floorPosition && obj1.getFloorPanel() == state.UP && obj.floorPosition > piston.getCurrentPosition())
+				if(obj1.floorPosition < obj2.floorPosition && obj1.returnPanelStatus() == state.UP && obj.floorPosition > piston.getCurrentPosition())
 					return 1;
-				else if(obj2.getFloorPanel() == state.UP && ob2.floorPosition > piston.getCurrentPosition())
+				else if(obj2.returnPanelStatus() == state.UP && ob2.floorPosition > piston.getCurrentPosition())
 					return -1;
 			
 			}
@@ -69,9 +69,9 @@ public class ControlSystem {
 					return 1;
 				if(obj1.getFloorPanelState() > obj2.getFloorPanelState())
 					return -1;
-				if(obj1.floorPosition > obj2.floorPosition && obj1.getFloorPanel() == state.DOWN && obj.floorPosition < piston.getCurrentPosition())
+				if(obj1.floorPosition > obj2.floorPosition && obj1.returnPanelStatus() == state.DOWN && obj.floorPosition < piston.getCurrentPosition())
 					return 1;
-				else if(obj2.getFloorPanel() == state.DOWN && ob2.floorPosition < piston.getCurrentPosition())
+				else if(obj2.returnPanelStatus() == state.DOWN && ob2.floorPosition < piston.getCurrentPosition())
 					return -1;
 			}
 			if(piston.getPistonState() == object.STATIONARY)

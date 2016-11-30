f53fbaf029bcc089b143738f87dfb186d0d6fa2e
jwd2 <jwd2@hood.edu>
2016-11-30 15:46:13 -0500

    Create ElevatorFan.java

---
+++ ElevatorFan.java
@@ -0,0 +1,17 @@
+package Undergraduate_Project;
+
+	public class ElevatorFan {
+	
+	//Accessor 
+	public boolean fanOn(){
+		return fanOn;
+	}
+	//Mutator 
+	public void setFan(boolean state){
+		fanOn = state;
+	}
+	//Constructor
+	public elevatorFan(){
+		fanOn = false;
+	}
+}
